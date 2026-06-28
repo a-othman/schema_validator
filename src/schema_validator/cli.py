e35a74c68677f0ff3db03b29ba388d4ffe75a60c
ahmed.othman <ahmed.n.othman01@gmail.com>
2026-06-28 11:26:53 +0400

    added first cli command

---
+++ src/schema_validator/cli.py
@@ -0,0 +1,11 @@
+import argparse
+from schema_validator.main import add
+
+def main():
+    parser = argparse.ArgumentParser(description="add two numbers.")
+    parser.add_argument("a", type=float, help="First number.")
+    parser.add_argument("b", type=float, help="Second number.")
+    args = parser.parse_args()
+    result = add(args.a, args.b)
+    print(f"The result of adding {args.a} and {args.b} is {result}.")
+
