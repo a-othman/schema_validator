05114ca867a5861bb3ce6c938532e5f5597a423b
ahmed.othman <ahmed.n.othman01@gmail.com>
2026-07-05 12:16:26 +0400

    adding tests for schema

--- tests/add_test.py
+++ tests/add_test.py
@@ -1,27 +1,71 @@
-from schema_validator.main import add
-from schema_validator.cli import add_cli
-import os 
-import sys
-from io import StringIO
+from schema_validator.schema import ColumnInfo, TableSchema, DataType, FormatType
+import pytest
+
+def test_column_info_creation_with_only_required_args():
+    column_info = ColumnInfo(name="email", data_type=DataType.STRING, nullable=False)
+    assert column_info.name == "email"
+    assert column_info.data_type == DataType.STRING
+    assert column_info.nullable == False
+    assert column_info.format is None
+    assert column_info.description is None
+
+def test_table_schema_with_pk_none():
+    columns = [
+        ColumnInfo(name="id", data_type=DataType.INTEGER, nullable=False),
+        ColumnInfo(name="name", data_type=DataType.STRING, nullable=True)
+    ]
+    table_schema = TableSchema(name="test_table", columns=columns, primary_key=None)
+    assert table_schema.name == "test_table"
+    assert len(table_schema.columns) == 2
+    assert table_schema.description is None
+    assert table_schema.primary_key is None
+
+def test_primary_key_exists_in_columns():
+    columns = [
+        ColumnInfo(name="id", data_type=DataType.INTEGER, nullable=False),
+        ColumnInfo(name="name", data_type=DataType.STRING, nullable=True)
+    ]
+    with pytest.raises(ValueError, match= r"Primary key '.*' does not exist in the columns."):
+        TableSchema(name="test_table", columns=columns, primary_key="non_existent_column")
 
 
-def test_add():
-    assert add(2, 3) == 5
+def test_unique_column_names_per_table():
+    columns = [
+        ColumnInfo(name="id", data_type=DataType.INTEGER, nullable=False),
+        ColumnInfo(name="name", data_type=DataType.STRING, nullable=True),
+        ColumnInfo(name="id", data_type=DataType.STRING, nullable=False)  # duplicate column name but different type
+    ]
+    with pytest.raises(ValueError, match= "Column names must be unique within a table schema."):
+        TableSchema(name="test_table", columns=columns)
 
-def test_cli_entry_point():
-    # Set up the command-line arguments
-    os.environ['PYTHONPATH'] = os.getcwd()  # Ensure the current directory is in PYTHONPATH
-    original_stdout = sys.stdout
-    original_argv = sys.argv
-    try:
-        sys.stdout = StringIO()
-        sys.argv = ['pv', '20', '13']  # Simulate command-line arguments
-        add_cli()
-        # Get the output and check if it contains the expected result
-        output = sys.stdout.getvalue()
 
-        assert "The result of adding 20.0 and 13.0 is 33.0." in output
-    finally:
-        # Restore original stdout and argv
-        sys.stdout = original_stdout
-        sys.argv = original_argv
+def test_format_only_for_string_data_type():
+    with pytest.raises(ValueError, match= r"Format is only applicable for STRING data type. Got DataType.* instead."):
+        ColumnInfo(name="id", data_type=DataType.INTEGER, nullable=False, format=FormatType.EMAIL)
+
+
+def test_invalid_format_instance_of_format_type():
+    with pytest.raises(TypeError, match= r"Invalid format type: .* Must be an instance of FormatType Enum."):
+        ColumnInfo(name="name", data_type=DataType.STRING, nullable=False, format="not_a_format")
+
+def test_invalid_data_type_instance_of_data_type_enum():
+    with pytest.raises(TypeError, match= r"Invalid data type: .* Must be an instance of DataType Enum."):
+        ColumnInfo(name="id", data_type="not_a_data_type", nullable=False)
+
+def test_table_schema_creation_with_valid_inputs():
+    columns = [
+        ColumnInfo(name="id", data_type=DataType.INTEGER, nullable=False),
+        ColumnInfo(name="name", data_type=DataType.STRING, nullable=True)
+    ]
+    table_schema = TableSchema(name="test_table", columns=columns, primary_key="id")
+    assert table_schema.name == "test_table"
+    assert len(table_schema.columns) == 2
+    assert table_schema.primary_key == "id"
+
+def test_column_info_creation_with_valid_inputs():
+    column_info = ColumnInfo(name="email", data_type=DataType.STRING, nullable=False, format=FormatType.EMAIL, description="User email address")
+    assert column_info.name == "email"
+    assert column_info.data_type == DataType.STRING
+    assert column_info.nullable == False
+    assert column_info.format == FormatType.EMAIL
+    assert column_info.description == "User email address"
